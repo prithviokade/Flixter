7a415ac08f0bb08957c5a52a806568298815384a
Prithvi Okade <prithviro@fb.com>
2020-06-24 16:47:34 -0400

    Parsing JSON to Movie objects

---
+++ app/src/main/java/com/example/flixter/models/Movie.java
@@ -0,0 +1,43 @@
+package com.example.flixter.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+
+    String posterPath;
+    String title;
+    String overview;
+
+    // constructor: jsonObject to create Movie object
+    public Movie(JSONObject jsonObject) throws JSONException {
+        posterPath = jsonObject.getString("poster_path");
+        title = jsonObject.getString("title");
+        overview = jsonObject.getString("overview");
+    }
+
+    // convert movie JSON Array to list of Movie objects
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
+        List<Movie> movies = new ArrayList<>();
+        for (int i = 0; i < movieJsonArray.length(); i++) {
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+        }
+        return movies;
+    }
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath); // fix this so it isn't hardcoded
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+}
