1bc0f68246fbdd5961d72264a1ef4167428e383a
Prithvi Okade <prithviro@fb.com>
2020-06-24 23:27:54 -0400

    Added landscape view

--- app/src/main/java/com/example/flixter/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixter/adapters/MovieAdapter.java
@@ -1,6 +1,7 @@
 package com.example.flixter.adapters;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -64,7 +65,13 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
         public void bind(Movie movie) {
             tmTitle.setText(movie.getTitle());
             tmOverview.setText(movie.getOverview());
-            Glide.with(context).load(movie.getPosterPath()).into(imPoster);
+            String imageUrl;
+            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) { // landscape
+                imageUrl = movie.getBackdropPath();
+            } else { // portrait
+                imageUrl = movie.getPosterPath();
+            }
+            Glide.with(context).load(imageUrl).into(imPoster);
         }
     }
 }
--- app/src/main/java/com/example/flixter/models/Movie.java
+++ app/src/main/java/com/example/flixter/models/Movie.java
@@ -10,12 +10,14 @@ import java.util.List;
 public class Movie {
 
     String posterPath;
+    String backdropPath;
     String title;
     String overview;
 
     // constructor: jsonObject to create Movie object
     public Movie(JSONObject jsonObject) throws JSONException {
         posterPath = jsonObject.getString("poster_path");
+        backdropPath = jsonObject.getString("backdrop_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
     }
@@ -31,6 +33,10 @@ public class Movie {
 
     public String getPosterPath() {
         return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath); // fix this so it isn't hardcoded
+    }
+
+    public String getBackdropPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath); // fix this so it isn't hardcoded
     }
 
     public String getTitle() {
